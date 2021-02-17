12a3dcc4c495b672263ab8e5579d953f535b621a
Booyamando <filibs10@gmail.com>
2021-02-17 01:09:21 +0000

    added to model.py

--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50)), nullable=Flase)
+    lastname = dn.Column(db.String(50)), nullable=Flase)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -15,5 +17,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+
             # do not serialize the password, its a security breach
         }
