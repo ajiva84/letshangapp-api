bf30e9f3172b177c691084f87fee754084bd3f3d
Booyamando <filibs10@gmail.com>
2021-02-17 01:44:42 +0000

    added values to models

--- src/models.py
+++ src/models.py
@@ -4,8 +4,14 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(50)), nullable=Flase)
-    lastname = dn.Column(db.String(50)), nullable=Flase)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    zipcode = db.Column(db.String(50), nullable=False)
+    bday = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -19,6 +25,12 @@ class User(db.Model):
             "email": self.email,
             "firstname": self.firstname,
             "lastname": self.lastname,
+            "address": self.address,
+            "city": self.address,
+            "state": self.address,
+            "zipcode": self.address,
+            "bday": self.address,
+            "gender": self.address,
 
             # do not serialize the password, its a security breach
         }
